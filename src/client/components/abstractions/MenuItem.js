7122808c3adfae22d0e02fdaaf9f0d9537b4a245
Nicky Lai <nicky.lai@lloydsbanking.com>
2020-09-03 16:56:44 +0100

    refactor MenuItem

---
+++ src/client/components/abstractions/MenuItem.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const MenuItem = () => (
+  <li className="item">
+    <h2>Dummy item</h2>
+    <p>
+      <span className="dietary">ve</span>
+      <span className="dietary">v</span>
+      <span className="dietary">n!</span>
+    </p>
+    <button className="remove-item">x</button>
+  </li>
+);
+
+export default MenuItem;
