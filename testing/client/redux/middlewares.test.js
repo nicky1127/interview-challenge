50df2aa9e127d1e44ee5e920953bfafab39b4dde
Nicky Lai <nicky.lai@lloydsbanking.com>
2020-09-04 16:16:45 +0100

    minor change

---
+++ testing/client/redux/middlewares.test.js
@@ -0,0 +1,13 @@
+import * as middlewares from "client/redux/middlewares";
+
+describe("middleware.itemRemover", () => {
+  it("next and dispatch are called when itemRemover gets called", () => {
+    const store = {
+      getState: jest.fn(() => ({ selectedItems: [1001, 1002] }))
+    };
+    const next = jest.fn();
+    middlewares.itemRemover(store)(next)({ type: "REMOVE_ITEM", payload: 0 });
+    expect(next).toHaveBeenCalled();
+    expect(store.getState).toHaveBeenCalled();
+  });
+});
