8b033a8f5c27d31287832c25144e9946e3feeffd
Nicky Lai <nicky.lai@lloydsbanking.com>
2020-09-04 12:39:04 +0100

    flesh up MenuPreview and menuItem

--- src/client/components/layouts/MenuPreview.js
+++ src/client/components/layouts/MenuPreview.js
@@ -1,4 +1,7 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import MenuItem from "../abstractions/MenuItem";
 
 const MenuPreview = () => (
   <div data-testid="menuPreview">
@@ -44,4 +47,21 @@ const MenuPreview = () => (
   </div>
 );
 
-export default MenuPreview;
+MenuPreview.defaultProps = { items: null, ldgItems: false };
+
+MenuPreview.propTypes = {
+  items: PropTypes.array,
+  ldgItems: PropTypes.bool.isRequired
+};
+
+const mapStateToProps = state => {
+  const { items, ldgItems } = state;
+  return {
+    items,
+    ldgItems
+  };
+};
+
+const ConnectedMenuPreview = connect(mapStateToProps)(MenuPreview);
+
+export default ConnectedMenuPreview;
